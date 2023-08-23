ce783ff94941eb1ced165258b46c902b3245c001
Ko Oo <kooo12@users.noreply.github.com>
2023-08-23 16:54:31 +0630

    verify otp screen added

---
+++ lib/login_screen.dart
@@ -0,0 +1,87 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _phoneController = TextEditingController();
+  TextEditingController _otpController = TextEditingController();
+  bool _codeSent = false;
+  bool _loading = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: (_codeSent)
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _otpController,
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                      labelText: 'Enter OTP',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: const Text('Verify')),
+                  (_loading)
+                      ? const CircularProgressIndicator()
+                      : const SizedBox()
+                ],
+              )
+            : Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    controller: _phoneController,
+                    keyboardType: TextInputType.phone,
+                    decoration: const InputDecoration(
+                        labelText: 'Phone Number',
+                        border: OutlineInputBorder(),
+                        prefix: Text('09')),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        setState(() {
+                          _loading = true;
+                        });
+                        if (_phoneController.text.length > 5) {
+                          FirebaseAuth.instance.verifyPhoneNumber(
+                              phoneNumber: '+959${_phoneController.text}',
+                              verificationCompleted: (credential) {},
+                              verificationFailed: (exception) {},
+                              codeSent: (s, n) {
+                                setState(() {
+                                  _codeSent = true;
+                                  _loading = false;
+                                });
+                              },
+                              codeAutoRetrievalTimeout: (str) {});
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Please enter vaild number')));
+                        }
+                      },
+                      child: const Text('Get OTP')),
+                  (_loading)
+                      ? const CircularProgressIndicator()
+                      : const SizedBox()
+                ],
+              ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_test/home.dart';
+import 'package:firebase_test/login_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
@@ -14,7 +16,28 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Home(),
+      theme: ThemeData(colorScheme: ColorScheme.dark()),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          body: StreamBuilder<User?>(
+        stream: _isLogin(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          User? user = snapshot.data;
+          if (user == null) {
+            return const LoginScreen();
+          }
+          return const Home();
+        },
+      )),
     );
+  }
+
+  Stream<User?> _isLogin() {
+    return FirebaseAuth.instance.authStateChanges();
   }
 }
